959ea3c2989c0e5c6268e7cc4a82e58226882d7a
SebastiiianG <usgg1106@gmail.com>
2025-01-23 09:19:03 -0600

    Remove registration link from Welcome component; add Role, User, Order, CgBrand, Computer, UCommunity, OrderDevice, CgDependency, and CgKindObject controllers with basic resource methods

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -4,8 +4,43 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 
 <template>
     <div>
-        <h1>
-            Hello World
-        </h1>
+        <button v-if="$page.props.user.permissions.includes('create roles')">
+            Gestionar Roles
+        </button>
+        <button v-if="$page.props.user.permissions.includes('create academic areas')">
+            Gestionar Áreas Académicas
+        </button>
+        <button v-if="$page.props.user.permissions.includes('create dependencies')">
+            Gestionar Dependencias
+        </button>
+        <button v-if="$page.props.user.permissions.includes('create brands')">
+            Gestionar Marcas
+        </button>
+        <button v-if="$page.props.user.permissions.includes('create objects')">
+            Gestionar Tipo de Dispositivos
+        </button>
+        <button v-if="$page.props.user.permissions.includes('create orders')">
+            Gestionar Órdenes
+        </button>
+        <button v-if="$page.props.user.permissions.includes('create failures')">
+            Gestionar Tipos de Falla
+        </button>
+        <button v-if="$page.props.user.permissions.includes('create persons')">
+            Gestionar Tipos de Personas
+        </button>
+        <button v-if="$page.props.user.permissions.includes('create users')">
+            Gestionar Usuarios
+        </button>
     </div>
 </template>
+
+<style>
+button{
+    background-color: green;
+    color: white;
+    padding: 20px;
+    margin: 5px;
+    border-radius: 5px;
+}
+
+</style>
