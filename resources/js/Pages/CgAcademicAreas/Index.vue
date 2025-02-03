aaa94ab99513e98af18a22f5297f264795a02ef6
SebastiiianG <usgg1106@gmail.com>
2025-02-02 20:39:57 -0600

    Add academic areas index page and update navigation links with permissions (didn't work correctly with responsiveness)

---
+++ resources/js/Pages/CgAcademicAreas/Index.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'CgAcademicAreasIndex'
+}
+</script>
+<script setup>
+//Importa la plantilla desde app layout
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+
+</script>
+
+<template>
+    <AppLayout>
+
+    </AppLayout>
+</template>
