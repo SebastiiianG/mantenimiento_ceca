c80515f2689c036622d2f46940761f55c75cc895
SebastiiianG <usgg1106@gmail.com>
2025-04-30 12:29:53 -0600

    Update UserSeeder to correct user details and ensure unique user numbers

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,20 +17,30 @@ class UserSeeder extends Seeder
     {
         // Creación del usuario "admin"
         $admin = User::create([
-            'name' => 'Fátima Reynoso Oropeza', // Nombre del usuario
-            'email' => 'fareynoso207@gmail.com', // Email del usuario
+            'name' => 'María Guadalupe Badillo Domínguez', // Nombre del usuario
+            'email' => 'badillom@uaeh.edu.mx', // Email del usuario
             'password'=> Hash::make('admin'), // Contraseña encriptada
-            'user_number' => 464396, // Número de usuario único
+            'user_number' => 5373, // Número de usuario único
             'status' => true // Estado del usuario
         ]);
         $admin->assignRole('admin'); // Asignación del rol "admin" al usuario creado
 
         // Creación del usuario "editor"
         $editor = User::create([
-            'name' => 'Ulises Sebastian García García', // Nombre del usuario
-            'email' => 'ulsegaga03@gmail.com', // Email del usuario
+            'name' => 'Omar González Luna', // Nombre del usuario
+            'email' => 'omargluna@hotmail.com', // Email del usuario
             'password'=> Hash::make('editor'), // Contraseña encriptada
-            'user_number' => 401420, // Número de usuario único
+            'user_number' => 8219, // Número de usuario único
+            'status' => true // Estado del usuario
+        ]);
+        $editor->assignRole('editor'); // Asignación del rol "editor" al usuario creado
+
+        // Creación del usuario "editor"
+        $editor = User::create([
+            'name' => 'Gabriela Alejandra Cabrera Martínez', // Nombre del usuario
+            'email' => 'gaby__am@hotmail.com', // Email del usuario
+            'password'=> Hash::make('editor'), // Contraseña encriptada
+            'user_number' => 3848, // Número de usuario único
             'status' => true // Estado del usuario
         ]);
         $editor->assignRole('editor'); // Asignación del rol "editor" al usuario creado
